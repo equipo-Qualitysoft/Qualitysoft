5eee4b4dd1d25ca323a2efd17abe5e479f3633bf
NicoPer <perea-9710@hotmail.com>
2022-09-09 16:12:54 -0500

    probando cambios

--- appwebingresoegreso/src/main/java/co/gov/misiontic/qualitysoft/appwebingresoegreso/repositorios/UsuarioRepository.java
+++ appwebingresoegreso/src/main/java/co/gov/misiontic/qualitysoft/appwebingresoegreso/repositorios/UsuarioRepository.java
@@ -3,12 +3,13 @@ package co.gov.misiontic.qualitysoft.appwebingresoegreso.repositorios;
 import co.gov.misiontic.qualitysoft.appwebingresoegreso.entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
     @Query(value="SELECT * FROM Usuario e where empresa_id = ?1", nativeQuery = true)
     public abstract ArrayList<Usuario> findByusuario(Long id);
 }
