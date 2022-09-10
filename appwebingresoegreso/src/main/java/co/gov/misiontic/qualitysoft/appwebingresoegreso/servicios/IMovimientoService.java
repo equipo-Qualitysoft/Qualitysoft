9237aef63da13037e04a289548435fdc4883ed6c
Euler Basante <euler206@gmail.com>
2022-09-10 00:34:17 -0500

    correciones, se corrigio el metodo creataMovimiento de la clase MoviemintoService.

--- appwebingresoegreso/src/main/java/co/gov/misiontic/qualitysoft/appwebingresoegreso/servicios/IMovimientoService.java
+++ appwebingresoegreso/src/main/java/co/gov/misiontic/qualitysoft/appwebingresoegreso/servicios/IMovimientoService.java
@@ -7,8 +7,8 @@ import java.util.List;
 public interface IMovimientoService {
     public List<MovimientoDinero> getMovimientoDineroList();
     public List<MovimientoDinero> getMovimientoDineroListByEmpresa(Long idEmpresa);
-    public MovimientoDinero getMovimientoById(Long idEmpresa, Long idMovimiento);
+    public MovimientoDinero getMovimientoById(Long idMovimiento);
     public MovimientoDinero createMovimiento(Long idEmpresa,MovimientoDinero nuevoMovimiento);
-    public MovimientoDinero updateMovimiento(Long idEmpresa,Long idMovimiento,MovimientoDinero movimiento);
-    public Boolean deleteMovimiento(Long idEmpresa, Long idMovimiento);
+    public MovimientoDinero updateMovimiento(MovimientoDinero movimiento);
+    public Boolean deleteMovimiento(Long idMovimiento);
 }
