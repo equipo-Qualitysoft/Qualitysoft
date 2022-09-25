337adac080e25d3c9d178209424d3275f9fd4a1b
Euler Basante <euler206@gmail.com>
2022-09-25 18:54:38 -0500

    ok ver movimientos

--- appwebingresoegreso/src/main/java/co/gov/misiontic/qualitysoft/appwebingresoegreso/controladores/HomeController.java
+++ appwebingresoegreso/src/main/java/co/gov/misiontic/qualitysoft/appwebingresoegreso/controladores/HomeController.java
@@ -1,18 +1,27 @@
 package co.gov.misiontic.qualitysoft.appwebingresoegreso.controladores;
 
+import co.gov.misiontic.qualitysoft.appwebingresoegreso.entidades.MovimientoDinero;
+import co.gov.misiontic.qualitysoft.appwebingresoegreso.servicios.IMovimientoService;
+import co.gov.misiontic.qualitysoft.appwebingresoegreso.servicios.MovimientoService;
 import co.gov.misiontic.qualitysoft.appwebingresoegreso.servicios.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class HomeController {
-    UsuarioService usuarioService;
+    @Autowired
+    IMovimientoService movimientoService;
     Map<String, Object> usuarioLogueado;
+
+
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
         if (principal != null) {
@@ -21,11 +30,11 @@ public class HomeController {
             model.addAttribute("user", usuarioLogueado);
             //model.addAttribute("profile",  principal.getClaims());
         }
-        return "index.html";
+        return "index";
     }
 
     @GetMapping("/tablero")
-    public String tablero(Model model, @AuthenticationPrincipal OidcUser principal) {
+    public String tablero(Model model) {
         model.addAttribute("user",  usuarioLogueado);
         return "tablero";
     }
@@ -39,10 +48,18 @@ public class HomeController {
     public String usuario(){
         return "usuario";
     }
-    @GetMapping("/movimiento")
-    public String movimiento(){
-        return "movimiento";
+
+
+    @GetMapping("/verMovimiento")
+    public String viewMovimiento(Model model,@ModelAttribute("mensaje") String mensaje) {
+        List<MovimientoDinero> movimientos = this.movimientoService.getMovimientoDineroList();
+        model.addAttribute("movimientos",  movimientos);
+        model.addAttribute("mensaje",  mensaje);
+        return "MovimientoDinero/verMovimiento";
     }
+
+
+
     @GetMapping("/cliente-proveedor")
     public String clienteProveedor(){
         return "cliente-proveedor";
